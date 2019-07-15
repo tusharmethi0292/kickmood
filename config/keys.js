382893dc5958e0e02fe46af83a6e4db3dd241b9c
tusharmethi0292 <tusharmethi0293@gmail.com>
2019-07-15 22:20:43 +0530

    c1

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+
+module.exports = {
+    mongoURI: "mongodb+srv://kick:<password>@clusterkick-vobrw.mongodb.net/test?retryWrites=true&w=majority"
+  };
---
+++ models/User.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
+// Load input validation
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
+// Load User model
+const User = require("../../models/User");
+
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+// Create Schema
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+module.exports = User = mongoose.model("users", UserSchema);
