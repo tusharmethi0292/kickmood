582a7574448dcd192122812728c018b1eff3ff21
tusharmethi0292 <tusharmethi0293@gmail.com>
2019-07-22 08:39:22 +0530

    c7

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
-
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <h1>hello </h1>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </div>
+      </Router>
+    );
+  }
 }
-
 export default App;
