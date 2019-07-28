d7eb8eb4c21ca73f5ea5fd57bd9a21d80589cbd9
tusharmethi0292 <tusharmethi0293@gmail.com>
2019-07-28 15:46:36 +0530

    c15

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-    mongoURI: "mongodb+srv://kick:mood%40123@clusterkick-vobrw.mongodb.net/test?retryWrites=true&w=majority",
     
+    mongoURI: "mongodb+srv://kick:mood%40123@clusterkick-vobrw.mongodb.net/test?retryWrites=true&w=majority",
     secretOrKey: "secret"
 
 
