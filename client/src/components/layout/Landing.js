e42aa69c939dc4de5a15ff8592c7ec6d9faf44cd
tusharmethi0292 <tusharmethi0293@gmail.com>
2019-11-14 22:17:29 +0530

    C18

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -9,7 +9,7 @@ class Moodhappy extends React.Component {
         to="/happy"
 
       >
-        <img src={require('../../assets/happy.png')} alt="be happy" height="50px" width="50px"/>
+        <img src={require('../../assets/happy.png')} alt="be happy always" height="50px" width="50px"/>
       </Link>
 
     </div>
@@ -24,7 +24,7 @@ class Moodcool extends React.Component {
         to="/cool"
 
       >
-        <img src={require('../../assets/cool.png')} alt="be happy" height="50px" width="50px"/>
+        <img src={require('../../assets/cool.png')} alt="be cool" height="50px" width="50px"/>
       </Link>
 
     </div>
@@ -39,7 +39,7 @@ class Moodhelping extends React.Component {
         to="/helping"
 
       >
-        <img src={require('../../assets/helping.png')} alt="be happy" height="50px" width="50px"/>
+        <img src={require('../../assets/helping.png')} alt="get ready to help" height="50px" width="50px"/>
       </Link>
 
     </div>
@@ -49,18 +49,114 @@ class Moodhelping extends React.Component {
 
 class Moodhopeful extends React.Component {
   render() {
-    return <div className="cool">
+    return <div className="hopeful">
 
       <Link
-        to="/cool"
+        to="/hopeful"
 
       >
-        <img src={require('../../assets/hopeful.png')} alt="be happy" height="50px" width="50px"/>
+        <img src={require('../../assets/hopeful.png')} alt="Let's hope" height="50px" width="50px"/>
       </Link>
 
     </div>
   }
 };
+
+
+class Moodlove extends React.Component {
+  render() {
+    return <div className="love">
+
+      <Link
+        to="/love"
+
+      >
+        <img src={require('../../assets/love.png')} alt="the best thing" height="50px" width="50px"/>
+      </Link>
+
+    </div>
+  }
+};
+
+class Moodmotivation extends React.Component {
+  render() {
+    return <div className="motivation">
+
+      <Link
+        to="/motivation"
+
+      >
+        <img src={require('../../assets/motivated.png')} alt="I can" height="50px" width="50px"/>
+      </Link>
+
+    </div>
+  }
+};
+
+class Moodsurprised extends React.Component {
+  render() {
+    return <div className="surprised">
+
+      <Link
+        to="/surprised"
+
+      >
+        <img src={require('../../assets/surprised.png')} alt="Awwww...." height="50px" width="50px"/>
+      </Link>
+
+    </div>
+  }
+};
+
+
+class Moodthinking extends React.Component {
+  render() {
+    return <div className="thinking">
+
+      <Link
+        to="/thinking"
+
+      >
+        <img src={require('../../assets/thinking.png')} alt="Hmmmmm" height="50px" width="50px"/>
+      </Link>
+
+    </div>
+  }
+};
+
+
+class Moodsporty extends React.Component {
+  render() {
+    return <div className="sporty">
+
+      <Link
+        to="/sporty"
+
+      >
+        <img src={require('../../assets/sporty.png')} alt="feel the heat" height="50px" width="50px"/>
+      </Link>
+
+    </div>
+  }
+};
+
+class Moodspiritual extends React.Component {
+  render() {
+    return <div className="spiritual">
+
+      <Link
+        to="/spiritual"
+
+      >
+        <img src={require('../../assets/spiritual.png')} alt="the highest level" height="50px" width="50px"/>
+      </Link>
+
+    </div>
+  }
+};
+
+
+
 
 class Login extends React.Component {
   render() {
@@ -114,6 +210,16 @@ class Landing extends Component {
         <Login></Login>
         <Moodhappy></Moodhappy>
         <Moodcool></Moodcool>
+        <Moodhelping></Moodhelping>
+        <Moodlove></Moodlove>
+        <Moodthinking></Moodthinking>
+        <Moodthinking></Moodthinking>
+        <Moodmotivation></Moodmotivation>
+        <Moodsporty></Moodsporty>
+        <Moodsurprised></Moodsurprised>
+        <Moodhopeful></Moodhopeful>
+        <Moodspiritual></Moodspiritual>
+
       </React.Fragment>
     );
 
